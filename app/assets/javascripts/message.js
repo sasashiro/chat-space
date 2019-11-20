90739823c354ae6e0ffd50c33c55e056473c0303
sasashiro <s.zashiro@gmail.com>
2019-11-20 14:55:02 +0900

    Summary

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,35 @@
 $(function(){
+  function buildHTML(message){
+    // var attachImage = message.image ? `<img src="${message.image}" class="message-main__image">`
+    var insertImage = '';
+        if (message.image.url) {
+            insertImage = `<img src="${message.image.url}">`;
+        }
+    var html = `<div class="message">
+                  <div class="message-upper">
+                    <div class="message-upper__group">
+                      ${message.user}
+                    </div>
+                    <div class="message-upper__date">
+                      ${message.date}
+                    </div>
+                  </div>
+                  <div class="message-main">
+                    <p class="message-main__text">
+                      ${message.content}
+                    </p>
+                    ${insertImage}
+                  </div>
+                </div>`
+    return html;
+  }
+  function scroll_view() {
+    $('.messages').animate({ scrollTop: $(".messages")[0].scrollHeight }, 500);
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this)
     var url = $(this).attr('action');
-    console.log('hello')
     $.ajax({
       url: url,
       type: 'POST',
@@ -12,5 +38,15 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.form__message').val('');
+      $('.form__submit').prop('disabled', false);
+      scroll_view();
+    })
+    .fail(function(){
+      alert('error');
+    });
   })
-})
+});
