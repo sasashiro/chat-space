aa07ec363ba86338777c34bfff1eac3da879f464
sasashiro <s.zashiro@gmail.com>
2019-12-04 17:49:16 +0900

    Summary

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,5 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable,  :validatable
@@ -11,4 +7,8 @@ class User < ApplicationRecord
   has_many :messages
   has_many :group_users
   has_many :groups, through: :group_users
+#   def self.search(input, id)
+#     return nil if input == ""
+#     User.where(['name LIKE ?', "%#{input}%"] ).where.not(id: id).limit(10)
+#   end
 end
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,5 @@
 json.content    @message.content
-json.user_id    @message.user_id
-json.date       @message.created_at.strftime("%Y/%m/%d %H:%M")
 json.image      @message.image.url
+json.date       @message.created_at.strftime("%Y年%m月%d日 %H時%M分")
 json.user       @message.user.name
+json.id         @message.id
